b82ce179631fb554e5a6ec2e10a40a00e01fe634
Desh Deepak <deepakdeshmohanpur@gmail.com>
2023-02-06 19:30:44 +0530

    first commit

---
+++ src/main/java/com/company/AuthorBooKAPI/AuthorBooKApiApplication.java
@@ -0,0 +1,14 @@
+package com.company.AuthorBooKAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorBooKApiApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(AuthorBooKApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/AuthorBooKAPI/AuthorBooKApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.AuthorBooKAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorBooKApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
